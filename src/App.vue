488416289b39ea3025a87dcbcebe29395a6bc806
Sigit Hardianto <69493819+sihar7@users.noreply.github.com>
2025-02-27 23:44:39 +0700

    ang ang ang asu

--- src/App.vue
+++ src/App.vue
@@ -92,8 +92,8 @@
       class="relative z-10 flex-grow w-full max-w-7xl mx-auto mt-8 p-8 rounded-2xl shadow-lg backdrop-blur-md border border-white/20 hover:shadow-2xl hover:border-blue-400/50 transition-all duration-500 transform hover:-translate-y-1"
       :style="{
         backgroundImage: isDarkMode
-          ? 'url(assets/cartographer.png), linear-gradient(to bottom, #0a0a23, #1a1a2e)' /* Gradien gerhana bulan */
-          : 'url(assets/cartographer.png), linear-gradient(to bottom, #0091ff, #a4dcfc)', /* Gradien langit cerah */
+          ? 'url(https://www.transparenttextures.com/patterns/cartographer.png), linear-gradient(to bottom, #0a0a23, #1a1a2e)' /* Gradien gerhana bulan */
+          : 'url(https://www.transparenttextures.com/patterns/cartographer.png), linear-gradient(to bottom, #0091ff, #a4dcfc)', /* Gradien langit cerah */
         backgroundBlendMode: 'overlay',
       }"
     >
